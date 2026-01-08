506e17067e31b0f254dbdc45309050e369062185
Antoine <antoine.richard-capponi@epita.fr>
2026-01-08 21:51:21 +0100

    fix: ui problems in the attendance modal

--- components/onboarding/OnboardingBackButton.tsx
+++ components/onboarding/OnboardingBackButton.tsx
@@ -8,6 +8,7 @@ import AnimatedPressable from "@/ui/components/AnimatedPressable";
 const OnboardingBackButton = (props: {
   icon?: string;
   position?: 'left' | 'right';
+  isModal?: boolean;
 }) => {
   const router = useRouter();
   const insets = useSafeAreaInsets();
@@ -17,17 +18,17 @@ const OnboardingBackButton = (props: {
       onPress={() => router.back()}
       style={[
         {
-        position: 'absolute',
-        top: insets.top + 4,
-        zIndex: 200,
-        backgroundColor: '#ffffff42',
-        padding: 10,
-        borderRadius: 100,
+          position: 'absolute',
+          top: props.isModal ? 20 : insets.top + 4,
+          zIndex: 200,
+          backgroundColor: '#ffffff42',
+          padding: 10,
+          borderRadius: 100,
         },
         props.position === 'right' ? { right: 16 } : { left: 16 }
       ]}
     >
-      <Papicons name={props.icon ?? "ArrowLeft"} size={26} fill={"#fff"}/>
+      <Papicons name={props.icon ?? "ArrowLeft"} size={26} fill={"#fff"} />
     </AnimatedPressable>
   )
 }
